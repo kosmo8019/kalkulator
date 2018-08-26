0ce6ae8875a169d2776f4212cb06c1c799dea160
kosmo8019 <s.budziosz@hotmail.com>
2018-08-26 09:11:22 +0200

    ver 1

--- kalkulator/Form1.cs
+++ kalkulator/Form1.cs
@@ -20,7 +20,7 @@ namespace kalkulator
 
         string liczbaPierwsza;
         string liczbaDruga;
-        string wynik;
+        
         char dzialanie;
 
 
@@ -53,74 +53,91 @@ namespace kalkulator
 
         private void buttonWynik_Click(object sender, EventArgs e)
         {
+            switch (dzialanie)
+            {
+                case ('+'):
+                    textWynik.Text = (int.Parse(liczbaPierwsza) + int.Parse(liczbaDruga)).ToString();
+                    break;
 
+                case ('-'):
+                    textWynik.Text = (int.Parse(liczbaPierwsza) - int.Parse(liczbaDruga)).ToString();
+                    break;
+
+                case ('*'):
+                    textWynik.Text = (int.Parse(liczbaPierwsza) * int.Parse(liczbaDruga)).ToString();
+                    break;
+
+                case ('/'):
+                    textWynik.Text = (int.Parse(liczbaPierwsza) / int.Parse(liczbaDruga)).ToString();
+                    break;
+            }
         }
 
         private void button10_Click(object sender, EventArgs e)
         {
             funkcjaDzialanie(0);
-            textWynik.Text = " ";
+            textWynik.Text = "";
         }
 
         private void button9_Click(object sender, EventArgs e)
         {
             funkcjaDzialanie(9);
-            textWynik.Text = " ";
+            textWynik.Text = "";
         }
 
         private void button8_Click(object sender, EventArgs e)
         {
             funkcjaDzialanie(8);
-            textWynik.Text = " ";
+            textWynik.Text = "";
         }
 
         private void button7_Click(object sender, EventArgs e)
         {
             funkcjaDzialanie(7);
-            textWynik.Text = " ";
+            textWynik.Text = "";
 
         }
 
         private void button6_Click(object sender, EventArgs e)
         {
             funkcjaDzialanie(6);
-            textWynik.Text = " ";
+            textWynik.Text = "";
         }
 
         private void button5_Click(object sender, EventArgs e)
         {
             funkcjaDzialanie(5);
-            textWynik.Text = " ";
+            textWynik.Text = "";
 
         }
 
         private void button4_Click(object sender, EventArgs e)
         {
             funkcjaDzialanie(4);
-            textWynik.Text = " ";
+            textWynik.Text = "";
 
         }
 
         private void button3_Click(object sender, EventArgs e)
         {
             funkcjaDzialanie(3);
-            textWynik.Text = " ";
+            textWynik.Text = "";
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
             funkcjaDzialanie(2);
-            textWynik.Text = " ";
+            textWynik.Text = "";
         }
         private void button1_Click(object sender, EventArgs e)
         {
             funkcjaDzialanie(1);
-            textWynik.Text = " ";
+            textWynik.Text = "";
         }
 
         private void funkcjaDzialanie(int liczba)
         {
-            if (dzialanie == ' ')
+            if (dzialanie == '')
             {
                 liczbaPierwsza += liczba;
                 textWynik.Text = liczbaPierwsza;
